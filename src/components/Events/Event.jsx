57f7db6d9f0f4f421b80520f92c6982176589f42
simenforris <simenforris@gmail.com>
2021-09-30 12:00:51 +0200

    remove Image component and add Online logo component with optional style

--- src/components/Events/Event.jsx
+++ src/components/Events/Event.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import './Event.css';
 
-import Image from '../Image/Image';
+import { faCalendarAlt, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import OnlineLogo from './OnlineLogo';
+
+// TODO: classNames => styles.{class}
 
 function Event({event}) {
   const date = new Date(event.start_date);
   const formattedDate = `${date.getDate()}.${date.getMonth()}`;
 
+  const imageSrc = event.images[0]?.thumb ? `https://online.ntnu.no${event.images[0].thumb}` : false;
+
   return (
     <div className="event">
-      <Image className="thumb" url={event.images[0]?.thumb} color="red" />
-      <div className="info">
+      {imageSrc ?
+        <img className="thumb" src={imageSrc} alt="Event" /> :
+        <OnlineLogo className="thumb" fillColor="red" />
+      }
+      <div className="data">
         <h3 className="title">{event.title}</h3>
         {event.ingress_short.length > 0 && <div className="ingress">{event.ingress_short}</div>}
-        <span>{formattedDate}</span>
-        <span className="seats">{event.max_capacity ? `${event.number_of_seats_taken}/${event.max_capacity}` : '\u221E'}</span>
+        <div className="info">
+          <span className="info-line">
+            <FontAwesomeIcon icon={faCalendarAlt} /><span>{formattedDate}</span>
+          </span>
+          <span className="info-line">
+            <FontAwesomeIcon icon={faUser} /><span>{event.max_capacity ? `${event.number_of_seats_taken}/${event.max_capacity}` : '\u221E'}</span>
+          </span>
+        </div>
       </div>
     </div>
   );
