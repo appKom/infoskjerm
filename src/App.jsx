f33c89b220c9a64704466840c1b23c925e290e98
David Lekve <davidlek@stud.ntnu.no>
2021-10-24 16:12:12 +0200

    got busdata from entur api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-
 //import Bus from './components/Bus/Bus';
 import BusContainer from './components/Bus/BusContainer';
 import './App.css';
+import createEnturClient from '@entur/sdk';
+import {useState, useEffect} from 'react';
+
 
 function App() {
 
-  const [bus, setBus] = useState(initialState)
+  const enturClient = createEnturClient({
+    clientName: 'appkom-infoskjerm',
+  });
+
+  const [ bus,setBus ] = useState([]);
+
+  useEffect(() => {
+    const getGloshaugen=async() => {
+      const glos=await fetchBusDepartures();
+      setBus(glos);
+    };
+
+    getGloshaugen();
+  }, []);
+
+  //https://stoppested.entur.org/    NSR:StopPlace:44085 id til gløshaugen
+  const fetchBusDepartures = async() => {
+    const gloshaugen = await enturClient.getDeparturesFromStopPlace('NSR:StopPlace:44085');
+    return gloshaugen;
+  };
+
+  console.log(bus);
 
   return (
     <div className="App">
@@ -13,7 +36,5 @@ function App() {
     </div>
   );
 }
-
-
 
 export default App;
