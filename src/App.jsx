164d2381595ec0aa1aeb0225729bf868f7de869e
simenforris <simenforris@gmail.com>
2021-10-28 11:17:22 +0200

    add grid to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Card from './components/Card';
 import WeatherContainer from './components/Weather/WeatherContainer';
 
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <WeatherContainer weather={weather}/>
+    <div className="App">
+      <Card rowStart={1} rowSpan={2} colStart={2}>
+        <WeatherContainer />
+      </Card>
     </div>
   );
 }
