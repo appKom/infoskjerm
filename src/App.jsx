5412d3505fc26926ea39a68fcd2a0e3922dede43
simenforris <simenforris@gmail.com>
2021-10-28 12:07:25 +0200

    first draft grid palcements

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import Card from './components/Card';
 import WeatherContainer from './components/Weather/WeatherContainer';
 
 import './App.css';
+import EventList from './components/Events/EventList';
 
 function App() {
   return (
     <div className="App">
-      <Card rowStart={1} rowSpan={2} colStart={2}>
+      <Card rowStart={1} rowSpan={2} colStart={1}>
         <WeatherContainer />
+      </Card>
+      <Card rowStart={1} colStart={4} rowSpan={5} colSpan={3}>
+        <EventList eventSize={4} />
       </Card>
     </div>
   );
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './Card.css';
+
 const Card = ({ children, rowStart, rowSpan = 1, colStart, colSpan = 2 }) => {
   return (
-    <div style={{ gridRow: `${rowStart} / span ${rowSpan}`, gridColumn: `${colStart} / span ${colSpan}` }}>
+    <div className="card" style={{ gridRow: `${rowStart} / span ${rowSpan}`, gridColumn: `${colStart} / span ${colSpan}` }}>
       {children}
     </div>
   );
