8ac80b3763e04d57403c98ef56d465ff89d22f80
simenforris <simenforris@gmail.com>
2022-01-30 14:31:32 +0100

    change to react-query

--- src/components/Events/EventList.jsx
+++ src/components/Events/EventList.jsx
@@ -1,24 +1,20 @@
-import React, { useEffect, useState } from 'react';
 import { fetchEventsByStartDate } from '../../api/EventApi';
 import Event from './Event';
 import moment from 'moment';
+import { useQuery } from 'react-query';
 
 import './EventList.css';
 
 function EventList({ eventSize = 3 }) {
-  const [ events, setEvents ] = useState([]);
+  const { isLoading, isError, data } = useQuery('events', () => fetchEventsByStartDate());
 
+  if (isLoading || isError) return null;
 
-  useEffect(() => {
-    fetchEventsByStartDate().then((data) => {
-      const filteredEvents = data.results.reduce((events, event) => {
-        if (event && event.attendance_event && moment(event.attendance_event.registration_end).isAfter(moment())) {
-          events.push(event);
-        }
-        return events;
-      }, []);
-      setEvents(filteredEvents);
-    });
+  const events = data?.results.reduce((events, event) => {
+    if (event && event.attendance_event && moment(event.attendance_event.registration_end).isAfter(moment())) {
+      events.push(event);
+    }
+    return events;
   }, []);
 
   return (
