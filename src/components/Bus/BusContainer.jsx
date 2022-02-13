6e15a8b8233c35fbfbc3757079dba6bfba270c62
hannavestvik <hanna.m.vestvik@gmail.com>
2022-02-13 15:25:13 +0100

    faktisk doooone<3

--- src/components/Bus/BusContainer.jsx
+++ src/components/Bus/BusContainer.jsx
@@ -27,12 +27,14 @@ const BusContainer = ({stoppID, busstopp}) => {
     const gloshaugen = await enturClient.getDeparturesFromStopPlace(stoppID);
     let output=[];
     for(let i=0;i<5; i++){
-      output.push({
 
-        busnr: gloshaugen[i].serviceJourney.id.substring(19,gloshaugen[i].serviceJourney.id.indexOf('_')),
-        retning: gloshaugen[i].destinationDisplay.frontText,
-        tid: gloshaugen[i].aimedDepartureTime.substring(gloshaugen[i].aimedDepartureTime.indexOf('T')+1,gloshaugen[i].aimedDepartureTime.indexOf('+')-3)
-      });
+      if(gloshaugen[i].serviceJourney.id.substring(19,gloshaugen[i].serviceJourney.id.indexOf('_')).length<=2){
+        output.push({
+          busnr: gloshaugen[i].serviceJourney.id.substring(19,gloshaugen[i].serviceJourney.id.indexOf('_')),
+          retning: gloshaugen[i].destinationDisplay.frontText,
+          tid: gloshaugen[i].aimedDepartureTime.substring(gloshaugen[i].aimedDepartureTime.indexOf('T')+1,gloshaugen[i].aimedDepartureTime.indexOf('+')-3)
+        });
+      }
     }
     return output;
   };
@@ -43,7 +45,10 @@ const BusContainer = ({stoppID, busstopp}) => {
   //bus.map(busitem-><Bus bussnr=busitem.servisejourney.id retning="" tid=""></Bus>)
   return (
     <div className="tabellDiv">
-      <h3>{busstopp}</h3>
+      <div className="header">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/RWBA_Bus.svg/1200px-RWBA_Bus.svg.png" alt="bilde av bus" className="bildebus inline" style={{height:'155px', padding: '5px', marginBottom: '2px'}}/>
+        <h3 className="inline">{busstopp}</h3>
+      </div>
       <table className="tabell">
         {bus.map((busitem, index) => (
           <Bus key={index} tid={busitem.tid} bussnr={busitem.busnr} retning={busitem.retning}/>
