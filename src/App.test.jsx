d677df14fd5da0d0b261ead75c196bfe5a0e3247
Julian <julianao@stud.ntnu.no>
2024-03-14 18:25:08 +0100

    Readded tests

---
+++ src/App.test.jsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+
+test('renders learn react link', () => {
+  render(<div>learn react</div>);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
