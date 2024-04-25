3a7aedfe6877543b15861a56dfbae27d6c26e7fb
Anam <anamam@stud.ntnu.no>
2024-04-25 15:58:58 +0200

    i

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,6 +2,7 @@
 // import eventTypes from '../../eventTypes';
 import OnlineLogo from './Logo/OnlineLogo';
 import eventTypes from '../lib/eventTypes.js';
+import { Badge } from './Badge';
 
 
 
@@ -11,13 +12,17 @@ export function Event(){
     <div>
       <a href="vg.no" className ="block max-w-sm w-327 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         {imgSrc ?
-          <img className="rounded-t-lg h-182 w-327" src={imgSrc} alt="Event" />:
+          <img className="rounded-t-lg h-182 w-327" src={imgSrc} alt="Event" /> :
           <div className="rounded-t-lg h-182 w-327">
             <OnlineLogo  fillColor={eventTypes[event.event_type]?.color} />
           </div>
         }
         <div className='p-5'>
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Title: Dogs are cute </h5>
+          <div className='flex'>
+            <Badge title='100000' icon='people' />
+            < Badge title='01.07' icon='calender'/>
+          </div>
         </div>
       </a>
     </div>
