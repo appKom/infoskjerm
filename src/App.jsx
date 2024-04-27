ff43c56ad73b9332d0a12c47ca09f047760abda3
Julian <julianao@stud.ntnu.no>
2024-04-27 13:06:39 +0200

    Fill screen height

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import { EventCarousel } from './components/EventCarousel';
 import './index.css';
 
 function App() {
-
   return (
-    <div className='overflow-hidden dark:bg-[#111827]'>
+    <div className='overflow-hidden dark:bg-[#111827] h-screen flex flex-col'>
       <Header />
-      <div className='flex flex-col justify-between h-full'>
+      <div className='flex flex-col h-full'>
         <EventCarousel />
         <EventCarousel />
       </div>
