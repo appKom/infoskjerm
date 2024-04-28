6982ad34b95be330f4938b2dbd182e5ecdf6fb11
Julian <julianao@stud.ntnu.no>
2024-04-28 16:49:48 +0200

    testing

--- src/components/EventCarousel.jsx
+++ src/components/EventCarousel.jsx
@@ -5,7 +5,7 @@ export function EventCarousel({events}){
   return(
     <div className='px-8 py-5'>
       <h2 className="mb-5 text-4xl font-bold dark:text-white">Kommende arrangementer</h2>
-      <div className =" grid grid-cols-4 justify-between max-w-full gap-8" >
+      <div className ="grid justify-between max-w-full grid-cols-5 gap-8 " >
         {events.map((event) => <EventCard key={event.id} event={event} />)}
       </div>
     </div>
