4b9f8a8f46dd6c1dbd96eab473d6586fb4606bdd
Julian <julianao@stud.ntnu.no>
2024-04-28 22:37:06 +0200

    revert

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -28,7 +28,7 @@ export function EventCard({ event }) {
       <div ref={containerRef} className='flex flex-col justify-between flex-grow w-full gap-2 px-4 pt-2 pb-3 overflow-hidden'>
         <div>
           {event.title && <h5 className="w-full text-2xl font-bold tracking-tight line-clamp-1 dark:text-white">{event.title}</h5>}
-          {event.title && <p className="font-normal text-gray-700 dark:text-gray-400 line-clamp-1">{event.description}</p>}
+          {event.title && <p className="font-normal text-gray-700 dark:text-gray-400 line-clamp-2">{event.description}</p>}
         </div>
         <div ref={contentRef} className='flex w-full gap-1 scrolling-text'>
           {eventTypeName && eventTypeColorName && <Badge text={eventTypeName} leftIcon='star' color={eventTypeColorName} />}
