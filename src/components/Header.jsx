ca5481c6e6d3d13c2d0e7aae8168f6dd843485dd
Julian <julianao@stud.ntnu.no>
2024-04-28 17:27:55 +0200

    svart bekk logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,10 +18,10 @@ export function Header(){
       <img className="h-5/6 dark:hidden" src="/online/online_icon_blue.svg" alt="Online logo" />
       <img className="hidden h-5/6 dark:block" src="/online/online_icon_white.svg" alt="Online logo" />
 
-      <img className="px-4 h-5/6 border-x dark:border-gray-700 dark:hidden" src="/bekk/Bekk_navnetrekk_blå.svg" alt="Bekk logo" />
+      <img className="px-4 h-5/6 border-x dark:border-gray-700 dark:hidden" src="/bekk/Bekk_navnetrekk_svart.svg" alt="Bekk logo" />
       <img className="hidden px-4 h-5/6 border-x dark:border-gray-700 dark:block" src="/bekk/Bekk_navnetrekk_hvit.svg" alt="Bekk logo" />
 
-      <span className="text-6xl text-online-blue dark:text-white">{time}</span>
+      <span className="text-6xl dark:text-white">{time}</span>
     </div>
   );
 }
