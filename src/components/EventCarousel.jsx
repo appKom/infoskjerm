9e0e5c1341a3d096fd05922aa32b441ea7891832
Julian <julianao@stud.ntnu.no>
2024-04-28 19:12:29 +0200

    info om hvordan melde seg på

--- src/components/EventCarousel.jsx
+++ src/components/EventCarousel.jsx
@@ -4,10 +4,11 @@ export function EventCarousel({title, events}){
 
   return(
     <div className='px-8 py-5'>
-      <h2 className="mb-5 text-4xl font-bold dark:text-white">{title}</h2>
+      <div className="mb-5 text-4xl font-bold dark:text-white">{title}</div>
       <div className ="grid justify-between max-w-full grid-cols-4 gap-8 " >
         {events.map((event) => <EventCard key={event.id} event={event} />)}
       </div>
+      <div className="mt-5 text-xl dark:text-white">Meld deg på arrangementer via <span className='font-medium'>online.ntnu.no</span>.</div>
     </div>
   );
 }
