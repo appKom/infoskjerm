b9216fee745905775ea9fbad300eb4a7678ee34d
Julian <julianao@stud.ntnu.no>
2024-05-07 22:15:25 +0200

    🤔

--- src/api/eeee.js
+++ src/api/eeee.js
@@ -2,7 +2,7 @@ import moment from 'moment';
 
 const ONLINE_API_URL = 'https://old.online.ntnu.no/api/v1/';
 
-const fetchEventsByStartDate = async () => {
+const fetchEventsByStartDate = async() => {
   const apiUrl = ONLINE_API_URL + `events?event_start__gte=${moment().format('YYYY-MM-DDTHH:mm')}`;
 
   const response = await fetch(apiUrl);
@@ -10,6 +10,6 @@ const fetchEventsByStartDate = async () => {
     return await response.json();
   }
   throw response;
-}
+};
 
 export default fetchEventsByStartDate;
