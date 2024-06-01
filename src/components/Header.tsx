fc7fcdd9a0079ffdc1a3e31b4f45db9990b875fd
Julian <julianao@stud.ntnu.no>
2024-06-01 19:21:53 +0200

    🤔

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,6 @@ import moment from 'moment';
 
 const REFRESH_TIME = '03:00';
 
-const INTERVAL_SECOND = 1000;
 const MESSAGE_INTERVAL_MINUTES = 2;
 const MESSAGE_TIME_SECONDS = 10;
 const MESSAGE_CONTENT = 'Lykke til på eksamen alle A4-krigere!';
@@ -18,22 +17,22 @@ export function Header() {
     const timeInterval = setInterval(() => {
       setTime(moment().format('HH:mm:ss'));
 
-      const currentDateFormatted = moment().format('YYYY-MM-DD HH:mm');
+      const currentTime = moment().format('YYYY-MM-DD HH:mm');
       const lastRefreshTime = localStorage.getItem('lastRefreshTime');
-      if (moment().format('HH:mm') === REFRESH_TIME && currentDateFormatted !== lastRefreshTime) {
-        localStorage.setItem('lastRefreshTime', currentDateFormatted);
+      if (moment().format('HH:mm') === REFRESH_TIME && currentTime !== lastRefreshTime) {
+        localStorage.setItem('lastRefreshTime', currentTime);
         window.location.reload();
       }
-    }, INTERVAL_SECOND);
+    }, 1000);
 
     const messageInterval = setInterval(() => {
       setShowMessage(true);
       setMoveContent(true);
       setTimeout(() => {
         setMoveContent(false);
-        setTimeout(() => setShowMessage(false), INTERVAL_SECOND);
-      }, INTERVAL_SECOND * MESSAGE_TIME_SECONDS);
-    }, INTERVAL_SECOND * 60 * MESSAGE_INTERVAL_MINUTES);
+        setTimeout(() => setShowMessage(false), 1000);
+      }, 1000 * MESSAGE_TIME_SECONDS);
+    }, 1000 * 60 * MESSAGE_INTERVAL_MINUTES);
 
     return () => {
       clearInterval(timeInterval);
@@ -44,7 +43,7 @@ export function Header() {
   return (
     <div className='relative h-32 border-b-[1.5px] border-light-grey dark:border-gray-700'>
       {showMessage && (
-        <div className='absolute top-0 left-0 flex items-center justify-center w-full h-full text-6xl italic dark:text-white'>
+        <div className='absolute top-0 left-0 flex items-center justify-center w-full h-full text-6xl italic dark:text-white message'>
           {MESSAGE_CONTENT}
         </div>
       )}
@@ -52,10 +51,13 @@ export function Header() {
         <div className="flex items-center h-full gap-8 px-12 py-6">
           <img className="h-5/6 dark:hidden" src="/online/online_icon_blue.svg" alt="Online logo" />
           <img className="hidden h-5/6 dark:block" src="/online/online_icon_white.svg" alt="Online logo" />
+
           <img className="px-4 h-5/6 border-x-[1.5px] dark:border-gray-700 dark:hidden" src="/bekk/Bekk_navnetrekk_svart.svg" alt="Bekk logo" />
           <img className="hidden px-4 h-5/6 border-x dark:border-gray-700 dark:block" src="/bekk/Bekk_navnetrekk_hvit.svg" alt="Bekk logo" />
-          <span className="text-6xl">{time}</span>
+
+          <span className="text-6xl ">{time}</span>
         </div>
+
         <div className='flex h-full gap-5 px-4'>
           <SeasonalGraphic />
         </div>
