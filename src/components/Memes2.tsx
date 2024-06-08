2eba9ca8aa473ed203c5b2ae5decc5e224a40d10
Julian <julianao@stud.ntnu.no>
2024-06-08 19:49:02 +0200

    Loading pie chart

--- src/components/Memes2.tsx
+++ src/components/Memes2.tsx
@@ -2,6 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchMemes } from "../api/memesApi";
 import { useEffect, useState } from "react";
 
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 const REFETCH_INTERVAL_MINUTES = 15;
 const AMOUNT_OF_MEMES = 3;
 const MEME_SHOW_TIME_SECONDS = 10;
@@ -14,6 +17,7 @@ export const Memes2 = () => {
   });
 
   const [currentMeme, setCurrentMeme] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(MEME_SHOW_TIME_SECONDS);
 
   useEffect(() => {
     if (!data) {
@@ -22,10 +26,18 @@ export const Memes2 = () => {
 
     const interval = setInterval(() => {
       setCurrentMeme((currentMeme + 1) % data.length);
+      setTimeLeft(MEME_SHOW_TIME_SECONDS);
     }, MEME_SHOW_TIME_SECONDS * 1000);
 
-    return () => clearInterval(interval);
-  }, [data]);
+    const countdown = setInterval(() => {
+      setTimeLeft(prevTime => (prevTime > 0 ? prevTime - 0.1 : 0));
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+      clearInterval(countdown);
+    };
+  }, [data, currentMeme]);
 
   if (isLoading) return <div>memes loading...</div>;
   if (isError) return <div>memes error</div>;
@@ -35,19 +47,32 @@ export const Memes2 = () => {
     <div className='px-8 py-5'>
       <div className="mb-5 text-4xl font-bold dark:text-white">Siste memes</div>
       <div className="flex justify-between h-[800px]">
-        <div className="flex justify-center p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="relative flex justify-center p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
           <div>
-            <div className="flex items-center gap-4">
-            <img className="w-10 h-10 rounded-full" src={data[currentMeme].author_image} alt={data[currentMeme].author} />
-            <div className="font-medium dark:text-white">
-              <div>{data[currentMeme].author}</div>
-              <div className="text-sm text-gray-500 dark:text-gray-400">{formatDate(data[currentMeme].date)}</div>
-            </div>
+            <div className="flex items-center w-full">
+              <div className="flex items-center w-full gap-4">
+                <img className="w-10 h-10 rounded-full" src={data[currentMeme].author_image} alt={data[currentMeme].author} />
+                <div className="font-medium dark:text-white">
+                  <div>{data[currentMeme].author}</div>
+                  <div className="text-sm text-gray-500 dark:text-gray-400">{formatDate(data[currentMeme].date)}</div>
+                </div>
+              </div>
+              <CircularProgressbar
+                className="h-10"
+                value={timeLeft}
+                maxValue={MEME_SHOW_TIME_SECONDS}
+                strokeWidth={50}
+                styles={buildStyles({
+                  pathColor: '#0B5374',
+                  trailColor: '#fff',
+                  strokeLinecap: "butt",
+                })}
+              />
           </div>
-          <img src={data[currentMeme].url} className="h-[700px] mt-4 rounded" alt={`Meme ${currentMeme}`}/*  onError={handleImageError} */ />
+          <img src={data[currentMeme].url} className="h-[700px] mt-4 rounded" alt={`Meme ${currentMeme}`} />
           </div>
         </div>
-        <div className="flex flex-col items-end justify-between flex-1 h-full">
+        <div className="flex flex-col items-end justify-between h-full">
           {data && data.map((meme: { url: string | undefined; }, index: number) => (
             <div key={index} className={`h-[180px] overflow-hidden bg-white border rounded shadow dark:bg-gray-800 dark:border-gray-700 w-max ${index === currentMeme ? 'border-online-blue border-4' : ''}`}>
               <img src={meme.url} className="object-contain w-auto h-full max-w-full mx-auto" alt={`Meme ${index}`} />
@@ -83,4 +108,3 @@ const formatDate = (dateInput: string): string => {
     return `${diffDays} dager siden`;
   }
 }
-
