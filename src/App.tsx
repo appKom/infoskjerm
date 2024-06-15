51b8eb4597254a6eb043b45726929bd25ba0b00d
Julian <julianao@stud.ntnu.no>
2024-06-15 23:57:10 +0200

    divider darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       </div>
       <div className='relative flex justify-between px-28'>
         <LatestMemes />
-        <div className='relative w-1 -mt-10 border-l -z-10 border-light-grey'></div>
+        <div className='relative w-1 -mt-10 border-l border-light-grey dark:border-gray-700'></div>
         <LatestBlasts />
       </div>
     </div>
