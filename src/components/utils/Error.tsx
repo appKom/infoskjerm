a6d48cd742fbe9a9ba4d75760cf3fd648126d32f
Julian Ammouche Ottosen <42567826+julian-ao@users.noreply.github.com>
2024-09-09 13:37:30 +0200

    114 error & loading messages (#116)
    * La til liste med feil og loading meldinger. Blir valgt random melding fra de
    
    * fiks slack page høyde når det loader
    
    * typo

--- src/components/utils/Error.tsx
+++ src/components/utils/Error.tsx
@@ -1,13 +1,31 @@
-export const Error = () => {
+import { useState, useEffect } from "react";
+
+const errorTexts = [
+  "Det har skjedd en feil, helt sikkert dotkom :(",
+  "En nettverkskabel har knukket :(",
+  "Noe gikk galt, kanskje en server har tatt ferie :(",
+  "Noen har progget i fylla :(",
+  "En katt har gått over tastaturet :(",
+  "En feil har oppstått, siste commit ble gjort av dotkom :(",
+  "Nytt forskningsresultat: ballmers peak er en myte :(",
+]
+
+export const Error = (props: { text?: string }) => {
+  const [randomText, setRandomText] = useState('');
+
+  useEffect(() => {
+    setRandomText(errorTexts[Math.floor(Math.random() * errorTexts.length)]);
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-10 animate-pulse">
+    <div className="flex flex-col items-center justify-center h-full gap-10">
       <img
         src="/online/online_logo_blue.svg"
         width={300}
         height={100}
         alt="Online logo"
       />
-      <div className="text-xl dark:text-white">Det har skjedd en feil, helt sikkert dotkom...</div>
+      <div className="text-xl dark:text-white">{props.text ? props.text : randomText}</div>
     </div>
   );
 };
--- src/components/utils/Loading.tsx
+++ src/components/utils/Loading.tsx
@@ -1,6 +1,24 @@
+import { useState, useEffect } from "react";
+
+const loadingTexts = [
+  "Venter på dotkom...",
+  "Dataen er forsinket, sikkert bussen...",
+  "Laster... Kanskje vi ta en kaffe i mellomtiden?",
+  "Dataen må ha gått seg vill...",
+  "Holder på å overtale serveren...",
+  "Datapakkene tar den lange veien...",
+  "Teller bytes, ett sekund...",
+]
+
 export const Loading = (props: { text?: string, hideLogo?: boolean}) => {
+  const [randomText, setRandomText] = useState('');
+
+  useEffect(() => {
+    setRandomText(loadingTexts[Math.floor(Math.random() * loadingTexts.length)]);
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen gap-10 animate-pulse">
+    <div className="flex flex-col items-center justify-center w-full h-full gap-10 animate-pulse">
       {!props.hideLogo && (
         <img
           src="/online/online_logo_blue.svg"
@@ -9,7 +27,7 @@ export const Loading = (props: { text?: string, hideLogo?: boolean}) => {
           alt="Online logo"
         />
       )}
-      <div className="text-xl dark:text-white">{props.text ? props.text : 'Venter på dotkom...'}</div>
+      <div className="text-xl dark:text-white">{props.text ? props.text : randomText}</div>
     </div>
   );
 };
