5a46bbc50fd813e9f7731ac16036c1e2b9ae6641
Julian <julianao@stud.ntnu.no>
2024-09-23 21:01:33 +0200

    Bug icon

--- src/components/header/DebugQR.tsx
+++ src/components/header/DebugQR.tsx
@@ -1,7 +1,12 @@
+import { Bug } from 'lucide-react'
+
 export const DebugQR = () => (
   <div className='flex items-center justify-center h-full gap-3 p-2 bg-[#f3f4f6] dark:bg-[#212f4d] rounded-lg max-h-min'>
     <div className="text-center text-gray-600 dark:text-white">
-      <p className="font-semibold">Debug</p>
+      <div className="flex items-center justify-center gap-2">
+        <p className="font-semibold">Debug</p>
+        <Bug className="w-5" />
+      </div>
       <p className="text-sm">Skann for info</p>
     </div>
     <img className="hidden dark:block h-2/3" src="/debug/debug-white-qr-code.svg" alt="Debug QR Code" />
