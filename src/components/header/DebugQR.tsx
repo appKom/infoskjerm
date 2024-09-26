028ea0e2505bf94e44b36b330acae0ce6ba6c023
Julian <julianao@stud.ntnu.no>
2024-09-26 15:37:14 +0200

    More margin to online icon in qr code

--- src/components/header/DebugQR.tsx
+++ src/components/header/DebugQR.tsx
@@ -1,8 +1,8 @@
 import { Bug } from 'lucide-react'
 
 export const DebugQR = () => (
-  <div className='flex items-center justify-center h-full gap-3 p-2 bg-[#f3f4f6] dark:bg-[#212f4d] rounded-lg max-h-min'>
-    <div className="text-center text-gray-600 dark:text-white">
+  <div className='flex items-center justify-center h-full gap-3 p-1.5 bg-[#f3f4f6] dark:bg-[#212f4d] rounded-lg max-h-min'>
+    <div className="p-1 text-center text-gray-600 dark:text-white">
       <div className="flex items-center justify-center gap-2">
         <p className="font-semibold">Debug</p>
         <Bug className="w-5" />
