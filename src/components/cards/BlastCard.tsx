85e6a9027be59d77354d169112827f9fbccfd8c1
Julian Ammouche Ottosen <julian.ammouche@gmail.com>
2024-10-10 18:30:14 +0200

    Changed the wawy slack messages are displayed

--- src/components/cards/BlastCard.tsx
+++ src/components/cards/BlastCard.tsx
@@ -10,30 +10,32 @@ export const BlastCard = ({ blast }: { blast: BlastType }) => {
 
   return (
     <BaseCard width={1000}>
-      <div className="flex items-center w-full gap-4 px-4 py-3 border-b border-gray-200 dark:border-gray-700">
+      <div className='flex px-4 py-3 gap-4'>
         <img
-          className="w-12 h-12 rounded-full"
+          className="w-12 h-12 rounded-lg"
           src={blast.author_image}
           alt={blast.author}
         />
-        <div className="font-medium dark:text-white">
-          <div>{blast.author}</div>
-          <div className="text-gray-500 dark:text-gray-400">
-            {formatSlackDate(blast.date)}
+        <div>
+          <div className="flex gap-2 items-center font-medium dark:text-white">
+            <div className="font-medium">{blast.author}</div>
+            <div className="text-sm text-gray-500 dark:text-gray-400">
+              {formatSlackDate(blast.date)}
+            </div>
           </div>
+          <div className="my-2 text-2xl font-bold dark:text-white" dangerouslySetInnerHTML={{ __html: headerLine }} />
+          <div
+            ref={contentRef}
+            className="mb-2 text-lg break-words text-ellipsis dark:text-white line-clamp-5"
+            dangerouslySetInnerHTML={{ __html: formattedText }}
+          />
+          {isOverflowing && (
+            <div className="pt-0 pb-2 text-gray-500 dark:text-white">
+              Les resten på <span className="text-blue-500">#korktavla</span>
+            </div>
+          )}
         </div>
       </div>
-      <div className="mx-4 mt-3 mb-2 text-2xl font-bold dark:text-white" dangerouslySetInnerHTML={{ __html: headerLine }} />
-      <div
-        ref={contentRef}
-        className="mx-4 mb-2 text-lg break-words text-ellipsis dark:text-white line-clamp-3"
-        dangerouslySetInnerHTML={{ __html: formattedText }}
-      />
-      {isOverflowing && (
-        <div className="px-4 pt-0 pb-2 text-gray-500 dark:text-white">
-          Les resten på <span className="text-blue-500">#korktavla</span>
-        </div>
-      )}
     </BaseCard>
   );
 };
