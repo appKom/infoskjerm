c55abb7148dbd4918323492c6a0371b3236a00f2
Julian Ammouche Ottosen <julian.ammouche@gmail.com>
2024-10-20 22:06:51 +0200

    Remove unnecesary text-animation

--- frontend/src/components/pages/Napkom.tsx
+++ frontend/src/components/pages/Napkom.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion";
 import { StarSky } from "../napkom/StarSky";
 
 const AMOUNT_OF_STARS = 500;
@@ -6,27 +5,21 @@ const AMOUNT_OF_STARS = 500;
 export const NapkomPage = () => (
   <div className="h-full flex justify-around items-center relative overflow-hidden w-full">
     <StarSky starCount={AMOUNT_OF_STARS} />
+
     {/* Dingling Jon Blund */}
     <img src="/napkom/jon-blund.png" alt="Jon Blund" className="absolute top-0 right-64 w-56 animate-swing" />
 
     {/* Napkom logo */}
-    <img src="/napkom/NapKom.svg" alt="Napkom logo" className="absolute bottom-10 right-10 w-32 z-30" />
+    <img src="/napkom/NapKom.svg" alt="Napkom logo" className="absolute bottom-8 right-8 w-32 z-30" />
     
     {/* Message */}
-    <div className="absolute inset-0 flex items-center justify-center">
-      <motion.div
-        className="z-20 text-white text-center"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 2, }}
-      >
-        <div className="text-8xl font-bold">
-          Sov godt i natt
-        </div>
-        <div className="text-3xl mt-10">
-          Presentert av <span className="text-online-yellow font-bold">Napkom</span>, i samarbeid med <span className="text-online-yellow font-bold">Jon Blund</span>.
-        </div>
-      </motion.div>
+    <div className="absolute inset-0 flex items-center flex-col justify-center z-20 text-white">
+      <div className="text-8xl font-bold">
+        Sov godt i natt
+      </div>
+      <div className="text-3xl mt-10">
+        Presentert av <span className="text-online-yellow font-bold">Napkom</span>, i samarbeid med <span className="text-online-yellow font-bold">Jon Blund</span>.
+      </div>
     </div>
   </div >
 );
