22c69b99fdb4498a4cedc489da87de0f7097dda2
Jørgen Galdal <jorgen.4@live.no>
2024-10-21 17:07:32 +0200

    feat: add snoring

--- frontend/src/components/pages/Napkom.tsx
+++ frontend/src/components/pages/Napkom.tsx
@@ -14,13 +14,25 @@ export const NapkomPage = () => (
 
     {/* Message */}
     <div className="absolute inset-0 flex items-center flex-col justify-center z-20 text-white">
-      <img src="/napkom/JonBlund.png" alt="" className="h-2/5 m-10 shadow-2xl" style={{ boxShadow: "0 2em 4em 1em black" }} />
-      <div className="text-8xl font-bold">
+      <img src="/napkom/JonBlund.png" alt="" className="h-2/5 m-10" style={{ boxShadow: "0 2em 4em 1em black" }} />
+
+      <div className="relative text-8xl font-bold">
         Sov godt i natt
+        {[0, 2, 4].map((animationOffset, index) => <p
+          key={index}
+          className="absolute text-white text-5xl -right-5 -top-5"
+          style={{
+            animationName: "driftOff",
+            animationDuration: "6s",
+            animationDelay: `${animationOffset}s`,
+            animationIterationCount: "infinite",
+            animationTimingFunction: "linear"
+          }}>Z</p>)}
       </div>
       <div className="text-3xl mt-10">
         Presentert av <span className="text-online-yellow font-bold">Napkom</span>, i samarbeid med <span className="text-online-yellow font-bold">Jon Blund</span>.
       </div>
+
     </div>
   </div >
 );
