1cea70f9c7a370693e5642d5894cb0a2ec84abae
Jørgen Galdal <jorgen.4@live.no>
2024-10-21 16:06:47 +0200

    fix: Bruke Debug-logo

--- frontend/src/components/header/DebugQR.tsx
+++ frontend/src/components/header/DebugQR.tsx
@@ -1,11 +1,10 @@
-import { Bug } from 'lucide-react'
-
 export const DebugQR = () => (
   <div className='flex items-center justify-center h-full gap-3 p-1.5 bg-[#f3f4f6] dark:bg-[#212f4d] rounded-lg max-h-min'>
+    <img src="/debug/debug-logo.svg" alt="Debug-logo"
+      className="h-2/3" />
     <div className="p-1 text-center text-gray-600 dark:text-white">
       <div className="flex items-center justify-center gap-2">
         <p className="font-semibold">Debug</p>
-        <Bug className="w-5" />
       </div>
       <p className="text-sm">Skann for info</p>
     </div>
