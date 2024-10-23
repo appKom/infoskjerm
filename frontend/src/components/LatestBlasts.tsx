8f9e0575b97ef2d1a4d91941de069fce55a2671d
fredrir <fhansteen@gmail.com>
2024-10-23 12:25:38 +0200

    added skelton loaders to slack page

--- frontend/src/components/LatestBlasts.tsx
+++ frontend/src/components/LatestBlasts.tsx
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import { BlastType } from "../lib/types";
 import { BlastCard } from "./cards/BlastCard";
 import { fetchBlasts } from "../api/blastsApi";
-import { Loading } from "./utils/Loading";
 import { Error } from "./utils/Error";
 
 const REFETCH_INTERVAL_MINUTES = 15; // how often to refetch blasts from slack
@@ -10,23 +9,37 @@ const AMOUNT_OF_BLASTS = 5; // how many blasts to fetch
 
 export const LatestBlasts = () => {
   const { isLoading, isError, data } = useQuery({
-    queryKey: ['blasts'],
+    queryKey: ["blasts"],
     queryFn: () => fetchBlasts(AMOUNT_OF_BLASTS),
-    refetchInterval: 1000 * 60 * REFETCH_INTERVAL_MINUTES
+    refetchInterval: 1000 * 60 * REFETCH_INTERVAL_MINUTES,
   });
 
-  if (isLoading) return <Loading text="Henter nyeste blæsts..." hideLogo />;
+  if (isLoading)
+    return (
+      <div className="my-8 flex flex-col gap-8 overflow-hidden w-max">
+        {[...Array(4)].map((_, index) => (
+          <div className="pr-24" key={index}>
+            <div className="flex flex-col items-center justify-center gap-4 text-center w-full h-full animate-pulse">
+              <div className="overflow-hidden bg-gray-700 w-[800px] h-64" />
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   if (isError) return <Error />;
 
-  if (data?.length === 0) return (
-    <p className='text-lg text-gray-500 dark:text-gray-400'>Her var det tomt :(</p>
-  )
+  if (data?.length === 0)
+    return (
+      <p className="text-lg text-gray-500 dark:text-gray-400">
+        Her var det tomt :(
+      </p>
+    );
 
   return (
-    <div className='my-8 flex flex-col gap-8 overflow-hidden w-max'>
-    {data?.map((blast: BlastType) => (
-      <BlastCard key={blast.id} blast={blast} />
-    )) || []}
+    <div className="my-8 flex flex-col gap-8 overflow-hidden w-max">
+      {data?.map((blast: BlastType) => (
+        <BlastCard key={blast.id} blast={blast} />
+      )) || []}
     </div>
   );
 };
--- frontend/src/components/LatestMemes.tsx
+++ frontend/src/components/LatestMemes.tsx
@@ -3,21 +3,20 @@ import { fetchMemes } from "../api/memesApi";
 import { MemeType } from "../lib/types";
 import { MemeCard } from "./cards/MemeCard";
 import { InfiniteAnimate } from "./utils/InfiniteAnimate";
-import { Loading } from "./utils/Loading";
 import { Error } from "./utils/Error";
 import { useEffect, useState } from "react";
 
 const REFETCH_INTERVAL_MINUTES = 60; // how often to refetch memes from slack
 const AMOUNT_OF_MEMES = 5; // how many memes to fetch
-const SPEED = .3; // how fast the memes should move
+const SPEED = 0.3; // how fast the memes should move
 
 const TRAINLENGTH = 3; // how many duplicated meme-lists to show for the infinite scroll effect
 
 export const LatestMemes = () => {
   const { isLoading, isError, data } = useQuery({
-    queryKey: ['memes'],
+    queryKey: ["memes"],
     queryFn: () => fetchMemes(AMOUNT_OF_MEMES),
-    refetchInterval: 1000 * 60 * REFETCH_INTERVAL_MINUTES
+    refetchInterval: 1000 * 60 * REFETCH_INTERVAL_MINUTES,
   });
 
   // TEMPORARY START - until video frontend is finished
@@ -25,28 +24,41 @@ export const LatestMemes = () => {
 
   useEffect(() => {
     if (data) {
-      const filteredData = data.filter(item => item?.type === 'image');
+      const filteredData = data.filter((item) => item?.type === "image");
       setFilteredMemes(filteredData);
     }
   }, [data]);
   // TEMPORARY END
 
-  if (isLoading) return <Loading text="Mekker de ferskeste memesa..." hideLogo />;
+  if (isLoading)
+    return (
+      <InfiniteAnimate axis="y" speed={SPEED} trainLength={TRAINLENGTH}>
+        {[...Array(4)].map((_, index) => (
+          <div className="px-5" key={index}>
+            <div className="flex flex-col items-center justify-center gap-4 text-center w-full h-full animate-pulse">
+              <div className="overflow-hidden bg-gray-700 h-48 w-64 lg:w-96 lg:h-64" />
+            </div>
+          </div>
+        ))}
+      </InfiniteAnimate>
+    );
+
   if (isError) return <Error />;
 
-  if (filteredMemes?.length === 0) return (
-    <p className='text-lg text-gray-500 dark:text-gray-400'>Her var det tomt :(</p>
-  )
+  if (filteredMemes?.length === 0)
+    return (
+      <p className="text-lg text-gray-500 dark:text-gray-400">
+        Her var det tomt :(
+      </p>
+    );
 
   return (
-    <InfiniteAnimate
-      axis='y'
-      speed={SPEED}
-      trainLength={TRAINLENGTH}
-    >
-      {filteredMemes ? filteredMemes.map((meme: MemeType) => (
-        <MemeCard key={meme.id} meme={meme} />
-      )) : []}
+    <InfiniteAnimate axis="y" speed={SPEED} trainLength={TRAINLENGTH}>
+      {filteredMemes
+        ? filteredMemes.map((meme: MemeType) => (
+            <MemeCard key={meme.id} meme={meme} />
+          ))
+        : []}
     </InfiniteAnimate>
   );
 };
