2b80246fefdccc24d4a802b6a98d3bdcc93f4bde
Julian Ammouche Ottosen <42567826+julian-ao@users.noreply.github.com>
2024-10-24 15:15:25 +0200

    Added Vercel Analytics (#193)
    * Added Vercel Analytics
    
    * Indentation

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,15 +2,15 @@ import './index.css';
 import App from './App';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Analytics } from "@vercel/analytics/react"
 
 const queryClient = new QueryClient();
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 root.render(
-    <QueryClientProvider client={queryClient}>
-        <App />
-    </QueryClientProvider>
-    );
-
-
+  <QueryClientProvider client={queryClient}>
+    <Analytics />
+    <App />
+  </QueryClientProvider>
+);
