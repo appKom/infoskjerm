fa881b5bec77b307eb8a808ab4dcae1b074ec4f0
Julian Ammouche Ottosen <julian.ammouche@gmail.com>
2024-11-03 18:19:22 +0100

    Removed App-component

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,10 @@
 import './index.css';
-import App from './App';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Analytics } from "@vercel/analytics/react"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AppRedirect } from './components/pages/AppRedirect';
+import { MainPage } from './components/pages/MainPage';
 
 const queryClient = new QueryClient();
 
@@ -11,6 +13,11 @@ const root = createRoot(container);
 root.render(
   <QueryClientProvider client={queryClient}>
     <Analytics />
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/online-appen" element={<AppRedirect />} />
+      </Routes>
+    </Router>
   </QueryClientProvider>
 );
