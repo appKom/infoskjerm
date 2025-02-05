b45d7819d0b504e505abdae82291dda767421b92
fredrir <fhansteen@gmail.com>
2025-02-05 10:45:30 +0100

    remove recursive function and reduce calls to DB

--- backend/src/client.ts
+++ backend/src/client.ts
@@ -1,6 +1,5 @@
 import { fetchMedia } from "./media";
 
-import { fetchComments } from "./comments";
 import { channelsConfig } from "./channelsConfig";
 
 export const saveMedia = async ({ limit }: { limit: number }) => {
@@ -10,20 +9,4 @@ export const saveMedia = async ({ limit }: { limit: number }) => {
     ...channelsConfig.memes.map((channelId) => fetchMedia(channelId, limit)),
     ...channelsConfig.blasts.map((channelId) => fetchMedia(channelId, limit)),
   ]);
-};
-
-export const saveComments = async ({
-  postId,
-  parentId,
-  channelId,
-}: {
-  postId: string;
-  parentId: string;
-  channelId: string;
-}) => {
-  try {
-    await fetchComments(channelId, postId, parentId);
-  } catch (error) {
-    console.error("Error fetching comments:", error);
-  }
 };
