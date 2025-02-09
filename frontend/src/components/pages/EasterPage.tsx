9ee9f212d57cc14128e266b17b391d5bbfe8d6d7
Sylvia Suet Wai Yung <sylvia.yung@ntnu.no>
2025-02-09 15:29:09 +0100

    easter page

--- frontend/src/components/pages/EasterPage.tsx
+++ frontend/src/components/pages/EasterPage.tsx
@@ -1,4 +1,5 @@
 import Holidays from 'date-holidays';
+import { useEffect, useState } from 'react';
 // import {useState, useEffect} from 'react';
 
 const countdownToEaster = () => {
@@ -15,22 +16,41 @@ const countdownToEaster = () => {
   const easterEve = new Date(goodFridayDate);
   easterEve.setDate(goodFridayDate.getDate() + 1);
   console.log(easterEve);
-  
-  return easterEve;
-};
+
+  const diffTime = Math.abs(easterEve.getTime() - today.getTime());
+  return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  };
 
 
 export const EasterPage = () => {
   //Const for countdown, use usestate to set "today" as the current date
   // const [countdown, setCountdown] = useState(countdownToEaster());
   console.log(countdownToEaster());
+  const [daysUntilEaster, setDaysUntilEaster] = useState(countdownToEaster());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDaysUntilEaster(countdownToEaster());
+    }, 60000); // Recalculate every 1 minute
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="relative flex items-center justify-center h-full bg-gradient-to-b from-[#9acdf5] to-[#d1e3f1] dark:bg-none">
       <div className="w-full max-w-2xl relative">
         <div className="relative z-50 p-8 bg-white border border-gray-200 shadow-xl dark:bg-gray-800 dark:border-gray-700 rounded-3xl">
           <h1 className="mb-8 text-4xl font-bold text-center text-gray-800 dark:text-gray-100">Nedtelling til påske</h1>
+          <div className='text-center'>
+            <p className={"font-extrabold text-pink-500 text-[250px] leading-none mb-6"}>{daysUntilEaster}</p>
+            <p className='text-gray-600 dark:text-gray-300 text-3xl'>
+              {daysUntilEaster === 1 ? "dag" : "dager"} igjen!
+            </p>
+          </div>
         </div>
       </div>
+      {/* Background */}
+      <img className="w-full absolute" src="/easter/tulipfield.jpg" alt="" />
     </div>
   )
 };
