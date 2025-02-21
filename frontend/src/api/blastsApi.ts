9b79dde27b4b5830782644ec494351c399220da1
Julian Ammouche Ottosen <42567826+julian-ao@users.noreply.github.com>
2025-02-21 13:14:03 +0100

    Removed useless api key (#256)

--- frontend/src/api/blastsApi.ts
+++ frontend/src/api/blastsApi.ts
@@ -3,11 +3,7 @@ import { BlastType } from "../lib/types";
 export const fetchBlasts = async (amount: number): Promise<BlastType[]> => {
   const apiUrl = import.meta.env.VITE_BACKEND_URL + `/latest-blasts?count=${amount}`;
 
-  const response = await fetch(apiUrl, {
-    headers: {
-        'x-api-key': import.meta.env.VITE_BACKEND_API_KEY
-    }
-  });
+  const response = await fetch(apiUrl);
 
   if (response.ok) {
     return await response.json();
